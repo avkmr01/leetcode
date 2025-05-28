b490698e21d77edf38f58338158c0e0c982825ef
avkmr01 <117144782+avkmr01@users.noreply.github.com>
2025-05-28 12:36:28 +0000

    half solved leetcode problem 10th ("*":solved, ".":unsolved)

---
+++ solutions/10_regular_expression_matching.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        // solving '*' problem first and then including '.' problem.
+        // lastly check compatibility with combining both
+
+        int slen = s.length();
+        int plen = p.length();
+        int sum = 0;
+        bool flag = true;
+        char alpha = s[0];
+        int i=0,j=0;
+
+        while(i<slen){
+            // for * case take input string: s = aabbcc , p=a*b*c*
+            // for . case take input string: s = adasdfw, p=ada.w
+                if(s[i] == p[j]){
+                    sum += 1;
+                    cout<<s[i]<<sum;
+                } else if(j<plen && s[i] == p[j+2]){
+                    sum += 1;
+                    cout<<s[i]<<sum;
+                    j = j+2;
+                }
+                if (isalpha(p[j+1])){
+                    j++;
+                }
+                i++;
+        }
+        return sum/slen;
+
+    }
+};
